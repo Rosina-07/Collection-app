ecd94afdb988c8bc3f91cf1af85a1a6986d32f8d
rosina <rosemat563@gmail.com>
2024-03-21 15:16:24 +0000

    testing routes

--- tests/Feature/CarTest.php
+++ tests/Feature/CarTest.php
@@ -13,6 +13,7 @@ use Tests\TestCase;
 class CarTest extends TestCase
 {
     use DatabaseMigrations;
+
     /**
      * A basic feature test example.
      */
@@ -25,8 +26,8 @@ class CarTest extends TestCase
         $response->assertStatus(200)
             ->assertJson(function (AssertableJson $json) {
                 $json->hasAll(['message', 'data'])
-                        ->whereType('message', 'string')
-                    ->has('data',1, function (AssertableJson $json) {
+                    ->whereType('message', 'string')
+                    ->has('data', 1, function (AssertableJson $json) {
                         $json->hasAll(['id', 'model', 'year', 'mileage', 'cute'])
                             ->whereAllType([
                                 'id' => 'integer',
@@ -39,13 +40,44 @@ class CarTest extends TestCase
             });
     }
 
-    public function test_get_singleCar_success() {
+    public function test_car_search()
+    {
+        $car = Car::factory()->create();
+        $car->model = 'bmw';
+        $car->save();
+
+        $car2 = Car::factory()->create();
+        $car2->model = 'Lamborghini';
+        $car2->save();
+
+        $response = $this->getJson('/api/cars?search=bmw');
+
+         $response->assertStatus(200)
+             ->assertJson(function (AssertableJson $json) {
+                 $json->hasAll(['message', 'data'])
+                     ->whereType('message', 'string')
+                 ->has('data', 1, function (AssertableJson $json) {
+                     $json->hasAll(['id', 'model', 'year', 'mileage', 'cute'])
+                         ->whereAllType([
+                             'id' => 'integer',
+                             'model' => 'string',
+                             'year' => 'integer',
+                             'mileage' => 'integer',
+                             'cute' => 'integer'
+                         ])->where('model', 'bmw');
+                 });
+         });
+
+    }
+
+    public function test_get_singleCar_success()
+    {
         Car::factory()->create();
         $response = $this->getJson('/api/cars/1');
         $response->assertStatus(200)
             ->assertJson(function (AssertableJson $json) {
                 $json->hasAll(['message', 'data'])
-                        ->whereType('message', 'string')
+                    ->whereType('message', 'string')
                     ->has('data', function (AssertableJson $json) {
                         $json->hasAll(['id', 'model', 'year', 'mileage', 'cute'])
                             ->whereAllType([
@@ -58,4 +90,94 @@ class CarTest extends TestCase
                     });
             });
     }
+
+    public function test_create_car_invalid()
+    {
+        $response = $this->postJson('/api/cars', []);
+
+        $response->assertInvalid(['model', 'year']);
+    }
+
+    public function test_create_car_valid()
+    {
+        $response = $this->postJson('/api/cars', [
+            'model' => 'new model',
+            'year' => 2002,
+            'mileage' => 20000,
+            'cute' => 1
+        ]);
+
+        $response->assertStatus(201)
+            ->assertJson(function (AssertableJson $json) {
+                $json->hasAll(['message'])->whereType('message', 'string');
+            });
+        $this->assertDatabaseHas('cars', [
+            'model' => 'new model',
+            'year' => 2002,
+            'mileage' => 20000,
+            'cute' => 1
+        ]);
+    }
+
+    public function test_update_car_invalidID()
+    {
+        $response = $this->putJson('/api/cars/1', [
+            'model' => 'new model',
+            'year' => 2002,
+            'mileage' => 20000,
+            'cute' => 1
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJson(function (AssertableJson $json) {
+                $json->hasAll(['message'])->whereType('message', 'string');
+            });
+    }
+
+    public function test_update_car_invalidData()
+    {
+        Car::factory()->create();
+        $response = $this->putJson('/api/cars/1', []);
+        $response->assertInvalid(['model', 'year']);
+    }
+    public function test_update_car_success()
+    {
+        Car::factory()->create();
+
+        $response = $this->putJson('/api/cars/1', [
+            'model' => 'new model',
+            'year' => 2002,
+            'mileage' => 20000,
+            'cute' => 1
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJson(function (AssertableJson $json) {
+                $json->hasAll(['message'])->whereType('message', 'string');
+            });
+        $this->assertDatabaseHas('cars', [
+            'model' => 'new model'
+        ]);
+    }
+
+    public function test_delete_car_success()
+    {
+        $car = Car::factory()->create();
+        $car->model = 'missing';
+        $car->save();
+
+        $response = $this->deleteJson('/api/cars/1');
+
+        $response->assertOk()
+            ->assertJson(function (AssertableJson $json) {
+                $json->hasAll(['message'])
+                    ->whereType('message', 'string');
+            });
+
+        $this->assertDatabaseMissing('cars', [
+            'model' => $car->model
+        ]);
+    }
+
+
 }
